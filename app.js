c31c39ae81d5a1ef7d6c92fb4edd93921d28837c
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-06-24 15:43:51 +0200

    Implement user authentication with login and signup routes

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.locals.pluralize = require('pluralize');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use('/', indexRouter);
+app.use('/', authRouter);n
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
